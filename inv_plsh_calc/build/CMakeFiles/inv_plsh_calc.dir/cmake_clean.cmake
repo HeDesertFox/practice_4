a4c75b3e76ddb14c7a0af87bbf71e497571c5a02
HeDesertFox <heyh020426@gmail.com>
2023-10-02 23:25:35 +0800

    Changes to be committed:

---
+++ inv_plsh_calc/build/CMakeFiles/inv_plsh_calc.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inv_plsh_calc.dir/getch.cpp.obj"
+  "CMakeFiles/inv_plsh_calc.dir/getch.cpp.obj.d"
+  "CMakeFiles/inv_plsh_calc.dir/getop.cpp.obj"
+  "CMakeFiles/inv_plsh_calc.dir/getop.cpp.obj.d"
+  "CMakeFiles/inv_plsh_calc.dir/main.cpp.obj"
+  "CMakeFiles/inv_plsh_calc.dir/main.cpp.obj.d"
+  "CMakeFiles/inv_plsh_calc.dir/stack.cpp.obj"
+  "CMakeFiles/inv_plsh_calc.dir/stack.cpp.obj.d"
+  "inv_plsh_calc.exe"
+  "inv_plsh_calc.exe.manifest"
+  "inv_plsh_calc.pdb"
+  "libinv_plsh_calc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inv_plsh_calc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
